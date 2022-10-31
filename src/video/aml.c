87cad37958210dbcda2d2f14cccc99ad72449bb3
Cameron Gutman <aicommander@gmail.com>
2022-10-31 00:06:06 -0500

    Vero 4K WIP

--- src/video/aml.c
+++ src/video/aml.c
@@ -36,6 +36,13 @@
 static codec_para_t codecParam = { 0 };
 
 int aml_setup(int videoFormat, int width, int height, int redrawRate, void* context, int drFlags) {
+  codecParam.handle = -1;
+  codecParam.cntl_handle = -1;
+  codecParam.sub_handle = -1;
+  codecParam.audio_utils_handle = -1;
+  codecParam.video_pid = 0; // What is this?
+  codecParam.video_path = FRAME_BASE_PATH_AMVIDEO;
+  codecParam.display_mode = DISPLAY_MODE_AMVIDEO;
   codecParam.stream_type = STREAM_TYPE_ES_VIDEO;
   codecParam.has_video = 1;
   codecParam.noblock = 0;
@@ -58,10 +65,12 @@ int aml_setup(int videoFormat, int width, int height, int redrawRate, void* cont
         if (!(major > 3 || (major == 3 && minor >= 14)) && width == 1920 && height == 1080)
             codecParam.am_sysinfo.param = (void*) UCODE_IP_ONLY_PARAM;
       }
+      codecParam.decoder_type = DECODER_TYPE_SINGLE_MODE;
       break;
     case VIDEO_FORMAT_H265:
       codecParam.video_type = VFORMAT_HEVC;
       codecParam.am_sysinfo.format = VIDEO_DEC_FORMAT_HEVC;
+      codecParam.decoder_type = DECODER_TYPE_FRAME_MODE;
       break;
     default:
       printf("Video format not supported\n");
@@ -71,6 +80,8 @@ int aml_setup(int videoFormat, int width, int height, int redrawRate, void* cont
   codecParam.am_sysinfo.width = width;
   codecParam.am_sysinfo.height = height;
   codecParam.am_sysinfo.rate = 96000 / redrawRate;
+  codecParam.am_sysinfo.ratio = ((uint32_t)width << 16) | height;
+  codecParam.am_sysinfo.ratio64 = ((uint64_t)width << 32) | height;
   codecParam.am_sysinfo.param = (void*) ((size_t) codecParam.am_sysinfo.param | SYNC_OUTSIDE);
 
   int ret;
@@ -94,6 +105,12 @@ void aml_cleanup() {
 int aml_submit_decode_unit(PDECODE_UNIT decodeUnit) {
   int result = DR_OK;
   PLENTRY entry = decodeUnit->bufferList;
+  struct vdec_status status;
+  if (codec_get_vdec_state(&codecParam, &status) != 0)
+    printf("vdec_state failed\n");
+  else
+    printf("Stream info: %ux%ux%u | Errors: %u | Status: 0x%x\n",
+           status.width, status.height, status.fps, status.error_count, status.status);
   while (entry != NULL) {
     int api = codec_write(&codecParam, entry->data, entry->length);
     if (api != entry->length) {
@@ -112,5 +129,5 @@ DECODER_RENDERER_CALLBACKS decoder_callbacks_aml = {
   .setup = aml_setup,
   .cleanup = aml_cleanup,
   .submitDecodeUnit = aml_submit_decode_unit,
-  .capabilities = CAPABILITY_DIRECT_SUBMIT | CAPABILITY_SLICES_PER_FRAME(8),
+  .capabilities = CAPABILITY_DIRECT_SUBMIT,
 };
